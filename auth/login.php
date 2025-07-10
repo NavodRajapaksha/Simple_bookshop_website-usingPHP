68a85c87d97ba8e59447d259339d75c742d70da3
Dulshan Rajapaksha <129612778+DulshanRajapaksha@users.noreply.github.com>
2025-07-10 19:22:44 +0530

    header and footer
    create include folder and add header.php and footer.php

---
+++ auth/login.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/5c5946fe44.js" crossorigin="anonymous"></script>
+    <title>Bookstore</title>
+  </head>
+  <body>
+
+
+
+    <div class="container">
+
+
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form class="form-control mt-5">
+                    <h4 class="text-center mt-3"> Login </h4>
+                   
+                    <div class="">
+                        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+                        <div class="">
+                            <input type="email"  class="form-control" id="" value="">
+                        </div>
+                    </div>
+                    <div class="">
+                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+                        <div class="">
+                            <input type="password" class="form-control" id="inputPassword">
+                        </div>
+                    </div>
+                    <button class="w-100 btn btn-lg btn-primary mt-4" type="submit">login</button>
+
+                </form>
+            </div>
+        </div>
+ 
+   
+
+    </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="" crossorigin="anonymous"></script>
+  </body>
+ 
+</html>
---
+++ config/config.php
@@ -0,0 +1,21 @@
+<?php
+
+// host
+$host = 'localhost';
+
+// dbname
+$dbname = 'bookshop-simple';
+
+// username
+$username = 'root';
+
+// password
+$password = '';
+
+// connect to the database
+$conn = new mysqli($host, $username, $password, $dbname);
+
+
+
+
+?>
